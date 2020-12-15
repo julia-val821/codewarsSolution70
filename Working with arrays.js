276fd15d160bd1b625f378279d0def3bcd5e81bd
JuliaVal821 <julia.kolgatina@gmail.com>
2020-12-14 23:30:32 -0500

    Fixed mistake

---
+++ Working with arrays.js
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/5a4ff3c5fd56cbaf9800003e/train/javascript
+
+    function withoutLast(arr) {
+        let arr1 = arr.slice();
+        arr1.pop();
+        return arr1;
+    }
